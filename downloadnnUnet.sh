2c8fd3eca8644d2b735e74137cc700b8928798d8
wangshubo90 <34329769+wangshubo90@users.noreply.github.com>
2022-04-09 16:52:47 -0400

    Create downloadnnUnet.sh

---
+++ downloadnnUnet.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd $HOME/gitrepos
+git clone https://github.com/NVIDIA/DeepLearningExamples.git
+mv DeepLearningExamples/PyTorch/Segmentation/nnUNet nnUnet
+cd nnUnet
+
+wget --content-disposition https://api.ngc.nvidia.com/v2/models/nvidia/dle/nnunet_pyt_ckpt_2d_fold2_amp/versions/21.11.0/zip -O nnunet_pyt_ckpt_2d_fold2_amp_21.11.0.zip
+export nnzip=$(ls | grep nnunet*.zip)
+unzip $nnzip
+rm $nnzip
+export nnzip=$(ls | grep nnunet*.ckpt)
